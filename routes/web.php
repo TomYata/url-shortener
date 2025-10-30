ce9cf43ac6f4d69e639fde7c9c0f55cee9f66fc3
TomYata <tom@yata.fr>
2025-10-30 11:39:03 +0100

    Utilisation d'un groupe pour les routes protégées

--- routes/web.php
+++ routes/web.php
@@ -16,19 +16,20 @@ Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
 
-
-// Formulaire de création d'URL courte
-Route::get('/shorturl/create', [ShortUrlController::class, 'create'])->name('shorturl.create')->middleware('auth');
-// Soumission du formulaire
-Route::post('/shorturl/store', [ShortUrlController::class, 'store'])->name('shorturl.store')->middleware('auth');
-// Edition d'une URL courte
-Route::get('/shorturl/{id}/edit', [ShortUrlController::class, 'edit'])->name('shorturl.edit')->middleware('auth');
-Route::put('/shorturl/{id}', [ShortUrlController::class, 'update'])->name('shorturl.update')->middleware('auth');
-// Suppression d'une URL courte
-Route::delete('/shorturl/{id}', [ShortUrlController::class, 'destroy'])->name('shorturl.destroy')->middleware('auth');
-
-// Tableau de bord utilisateur
-Route::get('/dashboard', [ShortUrlController::class, 'dashboard'])->name('dashboard')->middleware('auth');
+// Routes protégées par authentification
+Route::group(['middleware' => ['auth']], function () {
+    // Formulaire de création d'URL courte
+    Route::get('/shorturl/create', [ShortUrlController::class, 'create'])->name('shorturl.create');
+    // Soumission du formulaire
+    Route::post('/shorturl/store', [ShortUrlController::class, 'store'])->name('shorturl.store');
+    // Edition d'une URL courte
+    Route::get('/shorturl/{id}/edit', [ShortUrlController::class, 'edit'])->name('shorturl.edit');
+    Route::put('/shorturl/{id}', [ShortUrlController::class, 'update'])->name('shorturl.update');
+    // Suppression d'une URL courte
+    Route::delete('/shorturl/{id}', [ShortUrlController::class, 'destroy'])->name('shorturl.destroy');
+    // Tableau de bord utilisateur
+    Route::get('/dashboard', [ShortUrlController::class, 'dashboard'])->name('dashboard');
+});
 
 // Redirection via l'URL courte
 Route::get('/{short}', [ShortUrlController::class, 'redirect'])->name('shorturl.redirect');
